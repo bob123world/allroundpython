1b4a55124e187b62c162a1f2967d31cff74aab53
Michael Deboeure <michael.deboeure@student.uantwerpen.be>
2019-10-17 15:32:25 +0200

    Addition of Celery Airways

---
+++ celery airways/airport.py
@@ -0,0 +1,10 @@
+import os
+
+class Airport():
+    def __init__(self, *args, **kwargs):
+        self.gates = gates
+        self.taxiway = []
+        self.airqueue = []
+        self.runway = None
+
+    
